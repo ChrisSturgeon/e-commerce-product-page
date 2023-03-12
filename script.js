47f66704a9613322333ad49db2857bb18165badf
csturgeon <sturgeon.chris@gmail.com>
2023-03-12 15:02:05 +0000

    Refactor image functions

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
+// Navigation element variables
 const hamburgerOpen = document.querySelector('#hamburger-open');
 const hamburgerClose = document.querySelector('#hamburger-close');
 const sideModal = document.querySelector('#side-modal');
 const sideBar = document.querySelector('#side-bar');
 
-// Prevent click from bubbling
-sideBar.addEventListener('click', (event) => {
-  event.stopPropagation();
-});
-
+// Displays mobile side-navigation bar
 function showSideNav() {
   sideModal.style.display = 'block';
   setTimeout(() => {
@@ -16,6 +13,7 @@ function showSideNav() {
   }, 1);
 }
 
+// Hides mobile side-navigation bar
 function hideSideNav() {
   sideBar.classList.remove('out');
   sideModal.classList.remove('modal-in');
@@ -24,83 +22,135 @@ function hideSideNav() {
   }, 250);
 }
 
+// Prevent click on mobile sidebar from bubbling
+sideBar.addEventListener('click', (event) => {
+  event.stopPropagation();
+});
+
+// Mobile navigation event listeners
 hamburgerOpen.addEventListener('click', showSideNav);
 hamburgerClose.addEventListener('click', hideSideNav);
 sideModal.addEventListener('click', hideSideNav);
 
-// Images
+// **** Images ****
 const imageURLS = [
   'image-product-1.jpg',
   'image-product-2.jpg',
   'image-product-3.jpg',
   'image-product-4.jpg',
 ];
-const mobileNextButton = document.querySelector('#mobile-next-image');
-const mobilePrevButton = document.querySelector('#mobile-prev-image');
-const mobileMainImage = document.querySelector('#mobile-main-image');
 
+// Image Variables
+const mobileMainImage = document.querySelector('#mobile-main-image');
+const desktopMainImage = document.querySelector('#desktop-main-image');
+const modalMainImage = document.querySelector('#modal-main-image');
 let currentImage = 0;
 
-function mobileNextImage() {
+// Updates all main images to current image
+function updateMainImages() {
+  mobileMainImage.src = `./imgs/${imageURLS[currentImage]}`;
+  desktopMainImage.src = `./imgs/${imageURLS[currentImage]}`;
+  modalMainImage.src = `./imgs/${imageURLS[currentImage]}`;
+}
+
+// Changes active thumbnail class to current image
+function updateThumbnails() {
+  document.querySelectorAll('.thumb-box.active').forEach((thumbnail) => {
+    thumbnail.classList.remove('active');
+  });
+
+  document
+    .querySelectorAll(`.thumb-box[value="${currentImage}"]`)
+    .forEach((thumbnail) => {
+      thumbnail.classList.add('active');
+    });
+}
+
+// Switches main image to next in order
+function nextImage() {
   if (currentImage === imageURLS.length - 1) {
     currentImage = 0;
   } else {
     currentImage++;
   }
-  mobileMainImage.src = `./imgs/${imageURLS[currentImage]}`;
+  updateMainImages();
+  updateThumbnails();
 }
 
-function mobilePrevImage() {
+// Switches main image to previous in order
+function prevImage() {
   if (currentImage === 0) {
     currentImage = 3;
   } else {
     currentImage--;
   }
-  mobileMainImage.src = `./imgs/${imageURLS[currentImage]}`;
+  updateMainImages();
+  updateThumbnails();
 }
 
-mobilePrevButton.addEventListener('click', mobilePrevImage);
-mobileNextButton.addEventListener('click', mobileNextImage);
-
-// Desktop Thumbnail Click
-function changeImage(e) {
+// Switches main image to clicked image
+function selectImage(e) {
+  e.stopPropagation();
   currentImage = e.target.getAttribute('image');
-
-  // Update main image
-  document.querySelector(
-    '#desktop-main-image'
-  ).src = `./imgs/${imageURLS[currentImage]}`;
-
-  // Change thumb active states
-  document.querySelector('.thumb-box.active').classList.remove('active');
-  document
-    .querySelector(`.thumb-box[value="${currentImage}"]`)
-    .classList.add('active');
+  updateMainImages();
+  updateThumbnails();
 }
 
-// Thumnail event listeners
-document.querySelectorAll('.thumb-box').forEach((thumb) => {
-  thumb.addEventListener('click', (e) => changeImage(e));
-});
-
+// Shows/hides image lightbox
 function toggleImagesModal(show) {
   if (show) {
     document.querySelector('#images-modal').style.display = 'flex';
     document.body.style.overflow = 'hidden';
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        toggleImagesModal(false);
+      }
+    });
   } else {
     document.querySelector('#images-modal').style.display = 'none';
     document.body.style.overflow = 'auto';
   }
 }
 
+// Add event listeners for opening and closing modal
 document
   .querySelector('#desktop-main-image')
   .addEventListener('click', () => toggleImagesModal(true));
 
+document
+  .querySelector('#desktop-main-image')
+  .addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      toggleImagesModal(true);
+    }
+  });
+
+// Prevent click on modal main image from closing modal
 document
   .querySelector('#images-modal')
   .addEventListener('click', () => toggleImagesModal(false));
 
 document.querySelector('#modal-main-image').addEventListener('click', (e) => {
   e.stopPropagation();
+});
+
+// Previous image event listeners
+document.querySelectorAll('.prev-image').forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.stopPropagation();
+    prevImage();
+  });
+});
+
+// Next image event listeners
+document.querySelectorAll('.next-image').forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.stopPropagation();
+    nextImage();
+  });
+});
+
+// Thumbnail event listeners
+document.querySelectorAll('.thumb-box').forEach((thumb) => {
+  thumb.addEventListener('click', (e) => selectImage(e));
 });
